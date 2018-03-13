ba5966f94c0a547cc9f7519241912c505d7a8a71
frmdstryr <frmdstryr@gmail.com>
2018-03-13 18:24:53 -0400

    Make folder

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 export TARGETS=("iphoneos iphonesimulator android/arm android/arm64 android/x86 android/x86_64")
 
 # Install android lib
+mkdir -p $PREFIX/android/
 cp -RL $RECIPE_DIR/android $PREFIX/android/$PKG_NAME
