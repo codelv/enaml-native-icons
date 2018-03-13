daf5d4ad01db43035c5ded27b0a2b72dbe4828eb
frmdstryr <frmdstryr@gmail.com>
2018-03-13 18:21:05 -0400

    Move to conda

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export TARGETS=("iphoneos iphonesimulator android/arm android/arm64 android/x86 android/x86_64")
+
+# Install android lib
+cp -RL $RECIPE_DIR/android $PREFIX/android/$PKG_NAME
